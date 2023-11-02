9696780784165f5d15cb4bb52fa24c2ea8fa1b4e
fmallari <fc.mallari@gmail.com>
2023-11-02 01:06:11 -0700

    Part One - Getting Started + Adding Validation

---
+++ data.sql
@@ -0,0 +1,17 @@
+DROP DATABASE IF EXISTS bookstore;
+CREATE DATABASE bookstore;
+
+\c bookstore;
+
+DROP TABLE IF EXISTS books;
+
+CREATE TABLE books (
+    isbn TEXT PRIMARY KEY,
+    amazon_url TEXT,
+    author TEXT,
+    language TEXT,
+    pages INTEGER,
+    publisher TEXT,
+    title TEXT,
+    year INTEGER 
+);
